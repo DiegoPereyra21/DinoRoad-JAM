ba86ba92392b31a79967399d2cad6469edb080af
DiegoPereyra21 <diegoromanpereyra2102@gmail.com>
2026-06-11 01:23:58 -0300

    add the proper functioning of the store

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -6,15 +6,38 @@ public class ButtonScript : MonoBehaviour
 {
     public Button buttonBuy;
     public Button buttonEquip;
-
+    //id para cada personaje
+    public string characterID;
     public int costCharacter = 15;
+
+    private void Start()
+    {
+        //al entrar en escena vemos que boton debe estar activo segun si esta desbloqueado el personaje o no
+        if (IsUnlocked())
+        {
+            buttonBuy.gameObject.SetActive(false);
+            buttonEquip.gameObject.SetActive(true);
+        }
+        else
+        {
+            buttonBuy.gameObject.SetActive(true);
+            buttonEquip.gameObject.SetActive(false);
+        }
+    }
+
 
     public void checkCoins()
     {
+        //En caso de ya estar desbloqueado directamente sale
+        if (IsUnlocked()) return;
+
         if (CoinManager.TotalCoins >= costCharacter)
         {
             CoinManager.TotalCoins -= 15;
+
+            UnlockCharacter();
             BuyCharacter();
+
             buttonBuy.gameObject.SetActive(false);
             buttonEquip.gameObject.SetActive(true);
         }
@@ -27,6 +50,20 @@ public class ButtonScript : MonoBehaviour
 
     public void BuyCharacter()
     {
-        Debug.Log("Character bought");
+        Debug.Log("Character bought" + characterID);
     }
+
+    private bool IsUnlocked()
+    {
+        //verificacion sobre si esta guardado como bloqueado o no, 0 bloqueado y 1 desbloqueado
+        return PlayerPrefs.GetInt("Unlocked_" + characterID, 0) == 1;
+    }
+
+    private void UnlockCharacter()
+    {
+        //una vez comprado, llama a esta funcion para que asigne desbloqueado al personaje
+        PlayerPrefs.SetInt("Unlocked_" + characterID, 1);
+        PlayerPrefs.Save();
+    }
+
 }
